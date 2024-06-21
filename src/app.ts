4b9052a7bf29f73226ae0251757a975319874574
Banjos <48971965+brunosilveiradosanjos@users.noreply.github.com>
2024-06-21 08:23:55 -0300

    feat(orm): prisma integration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify'
+// import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+// const prisma = new PrismaClient()
+// prisma.user.create({
+//   data: {
+//     nome: '123',
+//     email: 'asdsad',
+//   },
+// })
