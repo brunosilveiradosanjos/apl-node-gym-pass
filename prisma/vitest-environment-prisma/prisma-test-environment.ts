9bed1a0b98f1baac4a2b62c00030952eb45e097f
Banjos <48971965+brunosilveiradosanjos@users.noreply.github.com>
2024-08-05 23:02:00 -0300

    feat(test): setup prisma environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,33 @@
+import { PrismaClient } from '@prisma/client'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import 'dotenv/config'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string){
+  if(!process.env.DATABASE_URL){
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema',schema)
+  return url.toString()
+}
 
 export default <Environment | unknown>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseUrl(schema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
     return {
       async teardown() {
         console.log('Teardown')
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+        await prisma.$disconnect()
       },
     }
   },
